072dcad0a1c813c1c954dda070525392bfed1e5b
wangtao@epsit.cn <wangtao@epsit.cn>
2018-07-27 19:24:08 +0800

    Initial commit

---
+++ printdemo/src/main/java/com/epsit/printdemo/BluetoothActivity.java
@@ -0,0 +1,92 @@
+package com.epsit.printdemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class BluetoothActivity extends AppCompatActivity {
+    private Context context = null;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.context = this;
+        setTitle("蓝牙打印");
+        setContentView(R.layout.bluetooth_layout);
+        requestPermission();
+        this.initListener();
+    }
+
+    public void requestPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            //判断是否有权限
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                //请求权限
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+                //向用户解释，为什么要申请该权限
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    Toast.makeText(this, "shouldShowRequestPermissionRationale", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // TODO Auto-generated method stub
+        if (requestCode == 1) {
+            if (permissions[0].equals(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 用户同意使用该权限
+            } else {
+                // 用户不同意，向用户展示该权限作用
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                    //showTipDialog("用来扫描附件蓝牙设备的权限，请手动开启！");
+                    return;
+                }
+            }
+        }
+    }
+
+    private void initListener() {
+        ListView unbondDevices = (ListView) this
+                .findViewById(R.id.unbondDevices);
+        ListView bondDevices = (ListView) this.findViewById(R.id.bondDevices);
+        Button switchBT = (Button) this.findViewById(R.id.openBluetooth_tb);
+        Button searchDevices = (Button) this.findViewById(R.id.searchDevices);
+
+        BluetoothAction bluetoothAction = new BluetoothAction(this.context,
+                unbondDevices, bondDevices, switchBT, searchDevices,
+                BluetoothActivity.this);
+
+        Button returnButton = (Button) this
+                .findViewById(R.id.return_Bluetooth_btn);
+        bluetoothAction.setSearchDevices(searchDevices);
+        bluetoothAction.initView();
+
+        switchBT.setOnClickListener(bluetoothAction);
+        searchDevices.setOnClickListener(bluetoothAction);
+        returnButton.setOnClickListener(bluetoothAction);
+    }
+
+    //屏蔽返回键的代码:
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
